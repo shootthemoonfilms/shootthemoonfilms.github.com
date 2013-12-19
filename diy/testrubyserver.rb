0b1e911f5d2dade3e50c576c16db43d3c644c5e6
Jeff Buchbinder <jeffreymbuchbinder@gmail.com>
2013-12-18 22:53:22 -0500

    Adjust port

--- diy/testrubyserver.rb
+++ diy/testrubyserver.rb
@@ -5,7 +5,7 @@ include WEBrick
 dir = ENV['OPENSHIFT_REPO_DIR']
 
 config = {}
-config.update(:Port => 80)
+config.update(:Port => 8080)
 config.update(:BindAddress => ARGV[0])
 config.update(:DocumentRoot => dir + 'public')
 server = HTTPServer.new(config)
