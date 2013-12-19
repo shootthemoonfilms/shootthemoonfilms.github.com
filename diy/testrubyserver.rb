f70d50fdd743eae2f4f4b8be35325566b07fead5
Jeff Buchbinder <jeffreymbuchbinder@gmail.com>
2013-12-18 22:38:37 -0500

    Fix with instructions from https://raw.github.com/rezajatnika/reza.re/master/source/_posts/2013-06-14-octopress-on-openshift.markdown

--- diy/testrubyserver.rb
+++ diy/testrubyserver.rb
@@ -2,10 +2,12 @@
 require 'webrick'
 include WEBrick
 
+dir = ENV['OPENSHIFT_REPO_DIR']
+
 config = {}
-config.update(:Port => 8080)
+config.update(:Port => 80)
 config.update(:BindAddress => ARGV[0])
-config.update(:DocumentRoot => ARGV[1])
+config.update(:DocumentRoot => dir + 'public')
 server = HTTPServer.new(config)
 ['INT', 'TERM'].each {|signal|
   trap(signal) {server.shutdown}
